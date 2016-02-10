f51023902026d9a49ae5734f9fc68f1c25329fea
Lars Lokhoff <Lars@wcw-stud-145-109-6-167.wireless.uva.nl>
2016-02-10 12:17:51 +0100

    drawing ugly histo

--- Opdracht1/main.py
+++ Opdracht1/main.py
@@ -7,12 +7,13 @@ def main():
 	m = 255
 	img = cv2.imread('cameraman.png', 0)
 	histo = histogramEqualization(img, m)
-
+	draw_histogram(histo, m)
 
 def draw_histogram(data, m):
-	n, bins, patches = plt.hist(20, 50, normed=1, facecolor='green', alpha=0.75)
+	n, bins, patches = plt.hist(data, 10)
+	plt.show()
 
-def histogramEqualization(f, m, bins=100):
+def histogramEqualization(f, m, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ,1.0]):
     his, be = np.histogram(f, range=(0,m), bins=bins)
     his = his.astype(float)/sum(his)
     return np.interp(f, be[1:], np.cumsum(his))
