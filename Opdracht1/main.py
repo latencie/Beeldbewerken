943f492065aaf6a120783df80059ac4326f44191
Lars Lokhoff <Lars@wcw-stud-145-109-6-167.wireless.uva.nl>
2016-02-10 12:29:07 +0100

    eyo

--- Opdracht1/main.py
+++ Opdracht1/main.py
@@ -7,13 +7,13 @@ def main():
 	m = 255
 	img = cv2.imread('cameraman.png', 0)
 	histo = histogramEqualization(img, m)
-	draw_histogram(histo, m)
+	
+	cv2.imshow(histo, img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
-def draw_histogram(data, m):
-	n, bins, patches = plt.hist(data, 10)
-	plt.show()
 
-def histogramEqualization(f, m, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ,1.0]):
+def histogramEqualization(f, m, bins= 100):
     his, be = np.histogram(f, range=(0,m), bins=bins)
     his = his.astype(float)/sum(his)
     return np.interp(f, be[1:], np.cumsum(his))
