51b380647f370454f22103499a7047342d1de306
Lars Lokhoff <Lars@wcw-stud-145-109-5-217.wireless.uva.nl>
2016-02-11 13:49:07 +0100

    part 2 and 3 done

--- Opdracht1/main.py
+++ Opdracht1/main.py
@@ -4,19 +4,43 @@ import cv2
 import matplotlib.pyplot as plt
 
 def main():
+    #this part shows the cameraman original and the cameraman after equalization
     m = 255
     img = cv2.imread('cameraman.png', 0)
     histo = histogramEqualization(img, m)
-    show_image('Original', img, 'Equalized', histo)
+    show_images([img, histo])
+
+    #This part shows the same object from 4 angles equalized
+    img_angle_1 = cv2.imread('view1.jpg', 0)
+    histo_1 = histogramEqualization(img_angle_1, m)
+
+    img_angle_2 = cv2.imread('view2.jpg', 0)
+    histo_2 = histogramEqualization(img_angle_2, m)
+
+    img_angle_3 = cv2.imread('view3.jpg', 0)
+    histo_3 = histogramEqualization(img_angle_3, m)
+
+    img_angle_4 = cv2.imread('view4.jpg', 0)
+    histo_4 = histogramEqualization(img_angle_4, m)
+
+    show_images([histo_1, histo_2, histo_3, histo_4])
 
 
-def show_image(name, image, name2, image2):
-    cv2.imshow(name, image)
-    cv2.imshow(name2, image2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
+# This function displays the given images together with their titles
+def show_images(images, cm=plt.cm.gray, axis='off', titles=[]):
+    number_images = len(images)
+    fig = plt.figure()
 
+    for i, img in enumerate(images):
+        fig.add_subplot(1, number_images, i + 1)
+        plt.axis(axis)
+
+        if len(titles) != 0:
+            plt.suptitle(titles[0])
+            plt.title(titles[i+1])
+
+        plt.imshow(img, cmap=cm)
+    plt.show()
 
 def histogramEqualization(f, m, bins= 100):
     his, be = np.histogram(f, range=(0,m), bins=bins)
