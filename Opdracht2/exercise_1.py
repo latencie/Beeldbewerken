42b03225bc9d50df31e4caa2ef249f5e18feff07
Lars Lokhoff <Lars@wcw-stud-145-109-7-125.wireless.uva.nl>
2016-02-17 11:36:56 +0100

    push voor AJIT

--- Opdracht2/exercise_1.py
+++ Opdracht2/exercise_1.py
@@ -1,17 +1,27 @@
 from pylab import *
+import cv2
 
 def main():
 	x = arange(-100, 101)
 	y = arange(-100, 101)
 
-	X, Y = meshgrid(x, y)
+	Y, X = meshgrid(x, y)
 	A = 1
 	B = 2
 	V = 6*pi / 201
 	W = 4*pi / 201
 	F = A*sin(V*X) + B*cos(W*Y)
+	Fx = V*A*cos(V*X)
+	Fy = W*B*-sin(W*Y)
+	
+	show_image(F)
+	show_image(Fx)
+	show_image(Fy)
+	
+def show_image(F):
 	clf()
 	imshow(F, cmap=cm.gray)
+	show()
 
 if __name__ == "__main__":
 	main()
